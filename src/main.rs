4246eb61808aeaf0388f6fae5b31f82eb6d8c2dd
advokat11 <adwaitaman3@gmail.com>
2023-05-10 17:00:31 +0300

    New update

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,44 @@
-    use std::path::Path;
-    use std::process::Command;
-    use walkdir::{DirEntry, WalkDir};
-    use indicatif::{ProgressBar, ProgressStyle};
-    fn main() {
-        println!("==========================================");
-        println!("=============== AQ Drivers ===============");
-        println!("==========================================");
-        println!("\nWarning: Do not turn off your computer during the installation process.\n");    
-        let start_path = ".";
+use std::env;
+use std::process::Command;
+use walkdir::WalkDir;
+use indicatif::{ProgressBar, ProgressStyle};
+use rayon::prelude::*;
+use std::borrow::Cow;
+
+fn main() {
+    println!("==========================================");
+    println!("=============== AQ Drivers ===============");
+    println!("==========================================");
+    println!("\nWarning: Do not turn off your computer during the installation process.\n");
     
-        let inf_files_count = count_inf_files(start_path);
-        let progress_bar = ProgressBar::new(inf_files_count as u64);
-        progress_bar.set_style(
-            ProgressStyle::default_bar()
-                .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})")
-                .progress_chars("#>-"),
-        );
+    let current_dir = env::current_dir().unwrap();
+    let walker = WalkDir::new(&current_dir).into_iter();
     
-        for entry in WalkDir::new(start_path).into_iter().filter_map(|e| e.ok()) {
-            if is_inf_file(&entry) {
-                let path = entry.path();
-                install_inf_driver(&path);
+    let inf_files: Vec<_> = walker
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.file_name().to_string_lossy().ends_with(".inf"))
+        .collect();
+
+    let total_files = inf_files.len() as u64;
     
-                progress_bar.inc(1);
-            }
-        }
-    
-        progress_bar.finish_and_clear();
-        println!("Установка драйверов завершена.");
-    }
-    
-    fn is_inf_file(entry: &DirEntry) -> bool {
-        entry
-            .file_name()
-            .to_str()
-            .map_or(false, |s| s.ends_with(".inf"))
-            && entry.file_type().is_file()
-    }
-    
-    fn count_inf_files(start_path: &str) -> usize {
-        WalkDir::new(start_path)
-            .into_iter()
-            .filter_map(|e| e.ok())
-            .filter(|entry| is_inf_file(entry))
-            .count()
-    }
-    
-    fn install_inf_driver(inf_path: &Path) {
-        let _ = Command::new("pnputil")
+    let pb = ProgressBar::new(total_files);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos:>7}/{len:7} {msg}")
+        .progress_chars("#>-"));
+
+    inf_files.par_iter().for_each(|entry| {
+        let path = entry.path();
+        let message = format!("Устанавливаем драйвер: {:?}", path);
+
+        pb.set_message(Cow::Owned(message));
+        pb.inc(1);
+
+        let _ = Command::new("pnputil.exe")
             .arg("/add-driver")
-            .arg(inf_path)
+            .arg(path.as_os_str())
             .arg("/install")
             .output();
-    }
-    
+    });
+
+    pb.finish_with_message("Завершено");
+}
