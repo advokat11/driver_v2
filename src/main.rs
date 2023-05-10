fa1ec2d57175e2dbd3d65bab6f5fde7d57db3fc4
advokat11 <adwaitaman3@gmail.com>
2023-05-10 17:44:10 +0300

    Terminal size adapt

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::borrow::Cow;
 use std::sync::{atomic::{AtomicUsize, Ordering}, Arc};
 use std::process::{Command, ExitStatus, Stdio};
 use walkdir::{DirEntry, WalkDir};
+use std::io::Write;
 
 #[cfg(unix)]
 use std::os::unix::process::ExitStatusExt;
@@ -17,15 +18,17 @@ fn main() {
     println!("==========================================");
     println!("\nWarning: Do not turn off your computer during the installation process.\n");
 
-
-    let walker: Vec<DirEntry> = WalkDir::new(".")
+    let driver_list: Vec<DirEntry> = WalkDir::new(".")
         .into_iter()
         .filter_map(Result::ok)
         .filter(|e| e.file_name().to_string_lossy().ends_with(".inf"))
         .collect();
 
-    let num_inf_files = walker.len();
-    let pb = ProgressBar::new(num_inf_files as u64);
+    let num_drivers = driver_list.len() as u64;
+
+    let (term_width, _) = term_size::dimensions().unwrap_or((80, 20));
+
+    let pb = ProgressBar::new(num_drivers);
     pb.set_style(ProgressStyle::default_bar()
         .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta}) {msg}")
         .progress_chars("#>-"));
@@ -33,9 +36,10 @@ fn main() {
     let successful_installs = Arc::new(AtomicUsize::new(0));
     let failed_installs = Arc::new(AtomicUsize::new(0));
 
-    walker.into_par_iter().for_each(|entry| {
+    driver_list.into_par_iter().for_each(|entry| {
         let path = entry.path();
-        let message = format!("Устанавливаем драйвер: {:?}", path);
+        let driver_name = path.file_name().unwrap().to_string_lossy();
+        let message = format!("Устанавливаем драйвер: {}", driver_name);
         pb.set_message(Cow::Owned(message));
 
         let status = Command::new("pnputil.exe")
@@ -52,6 +56,9 @@ fn main() {
         } else {
             failed_installs.fetch_add(1, Ordering::SeqCst);
         }
+
+        print!("{:<1$}\r", "", term_width as usize);
+        std::io::stdout().flush().unwrap();
 
         pb.inc(1);
     });
