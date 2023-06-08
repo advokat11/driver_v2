e46bb765e7081f43debad3bc3e6e499e4233dd22
advokat11 <adwaitaman3@gmail.com>
2023-06-08 16:03:13 +0300

    new progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use indicatif::{ProgressBar, ProgressStyle};
-use std::borrow::Cow;
+use pbr::ProgressBar;
 use std::env;
 use std::sync::{atomic::{AtomicUsize, Ordering}, Arc};
 use std::process::{Command, ExitStatus, Stdio};
 use walkdir::{DirEntry, WalkDir};
-use std::io::{Write};
 use std::fs::File;
 
 #[cfg(unix)]
@@ -26,12 +24,7 @@ fn main() {
 
     let num_drivers = driver_list.len() as u64;
 
-    let (term_width, _) = term_size::dimensions().unwrap_or((80, 20));
-
-    let pb = ProgressBar::new(num_drivers);
-    pb.set_style(ProgressStyle::default_bar()
-        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta}) {msg}")
-        .progress_chars("#>-"));
+    let mut pb = ProgressBar::new(num_drivers);
 
     let successful_installs = Arc::new(AtomicUsize::new(0));
     let failed_installs = Arc::new(AtomicUsize::new(0));
@@ -42,11 +35,11 @@ fn main() {
         None
     };
 
-    driver_list.into_iter().for_each(|entry| {
+    for entry in driver_list.into_iter() {
         let path = entry.path();
         let driver_name = path.file_name().unwrap().to_string_lossy();
-        let message = format!("Installing driver: {}", driver_name);
-        pb.set_message(Cow::Owned(message.clone()));
+        let message = format!("Installing driver: {} ", driver_name);
+        pb.message(&message);
 
         let mut command = Command::new("pnputil.exe");
         command.arg("/add-driver")
@@ -69,16 +62,13 @@ fn main() {
             failed_installs.fetch_add(1, Ordering::SeqCst);
         }
 
-        print!("{:<1$}\r", "", term_width as usize);
-        std::io::stdout().flush().unwrap();
+        pb.inc();
+    }
 
-        pb.inc(1);
-    });
-
-    pb.finish_with_message("Done");
+    pb.finish_print("Done");
 
     println!(
-        "Successful installs: {}, Failed installs: {}",
+        " Successful installs: {}, Failed installs: {}",
         successful_installs.load(Ordering::SeqCst),
         failed_installs.load(Ordering::SeqCst),
     );
